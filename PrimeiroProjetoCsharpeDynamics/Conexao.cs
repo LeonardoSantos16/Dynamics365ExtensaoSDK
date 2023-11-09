a18f8e16e1aa3d5b17f465fb9a055451b3a7fddd
LeonardoSantos16 <87033299+LeonardoSantos16@users.noreply.github.com>
2023-11-08 22:20:48 -0300

    Update Conexao.cs

--- PrimeiroProjetoCsharpeDynamics/Conexao.cs
+++ PrimeiroProjetoCsharpeDynamics/Conexao.cs
@@ -11,15 +11,13 @@ namespace PrimeiroProjetoCsharpeDynamics
     public class Conexao // classe de conexão ao dynamics
     {
         private static CrmServiceClient crmServiceClientTreinamento; // armazena o conteúdo da conexão
-        // AppId = e61fc53c-656f-ee11-8178-0022483715b6;
-        // AppId = 6c540d8d-a99b-42e4-837a-93f59decc366
-        // RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+        
         public CrmServiceClient ObterConexao() // método que devolve uma CrmServiceCliente
             {
                  //variável contendo a ConnectionString
                    var connectionStringCRM = @"AuthType=OAuth;
-                   Username = 125111377116@ulife.com.br;
-                   Password = 280202;
+                   Username = emaildafacul@ulife.com.br;
+                   Password = senha;
                    AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
                    RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
                    Url = https://org73fcfb64.crm2.dynamics.com/main.aspx;";
@@ -54,36 +52,5 @@ namespace PrimeiroProjetoCsharpeDynamics
             }
             return crmServiceClientTreinamento;
         }
-    }
-
-            
-       /* public CrmServiceClient ObterConexao()
-        {
-            if (crmServiceClientTreinamento == null)
-            {
-                try
-                {
-                    System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                    var connectionStringCRM = "AuthType=OAuth;" +
-                        "Username=leodsmf_gmail.com#EXT#@leodsmfgmail.onmicrosoft.com;" +
-                        "Password=Cavalomanco21_; SkipDiscovery=True;" +
-                        "AppId=e61fc53c-656f-ee11-8178-0022483715b6;" +
-                        "RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;" +
-                        "Url=https://orgc7c356c7.crm2.dynamics.com/main.aspx;";
-
-                    crmServiceClientTreinamento = new CrmServiceClient(connectionStringCRM);
-                    Console.WriteLine("VASCO DA GAMA E NADA MAIS");
-                }
-                catch (Exception ex)
-                {
-                    // Lidar com a exceção, por exemplo, registrando-a ou lançando uma exceção personalizada.
-                    throw new Exception("Erro ao criar a conexão CRM: " + ex.Message);
-                }
-            }
-            return crmServiceClientTreinamento;
-        }
-       */
-    
-    
-
+    }  
 }
